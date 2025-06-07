5e4a6dd75dfe22608b335c3047ceddc9175b86be
Afrifa <isaac.afrifa3@yahoo.com>
2025-06-08 02:39:24 +0300

    feat: add and register function
    - Added starter-function-web maven dependency

--- src/main/java/com/iam/event_cast/EventCastApplication.java
+++ src/main/java/com/iam/event_cast/EventCastApplication.java
@@ -2,6 +2,10 @@ package com.iam.event_cast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
 
 @SpringBootApplication
 public class EventCastApplication {
@@ -10,4 +14,8 @@ public class EventCastApplication {
 		SpringApplication.run(EventCastApplication.class, args);
 	}
 
+@Bean
+	public Function<String, String> reverseString() {
+		return value -> new StringBuilder(value).reverse().toString();
+	}
 }
